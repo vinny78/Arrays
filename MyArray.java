3a74039a55cf0cc062216f22b3e792cc5edabf09
vinny78 <146063659+vinny78@users.noreply.github.com>
2024-02-06 07:35:02 +0100

    Add files via upload

--- MyArray.java
+++ MyArray.java
@@ -1,25 +1,76 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
 import java.util.Scanner;
 public class MyArray {
+    //Variable Global que se puede usar en varias funciones sin errores.
+    static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+
 
         int[] array;
-        array = new int[]{0, 1, 2, 3, 4, 123};
-        System.out.println(array[5]);
+        array = new int[]{230, 144, 122, 33, 44, 123};
+
+        int[] array1;
+        array1 = new int[]{230, 144, 122, 33, 44, 123};
+
+        int[] array2;
+        array2 = new int[]{230, 144, 122, 33, 44, 123};
+        /*
+        imprimirArray(array);
 
         System.out.println(maximoArray());
         System.out.println(minimoArray());
         System.out.println(mediaArray());
+
+        boolean esIgual = sonIguales();
+        if (esIgual == true) {
+            System.out.println("El numero es igual");
+        } else {
+            System.out.println("No coincide");
+        }
+
+        int[] resultadoVectoresSuma = sumaVectores();
+        for (int i = 0; i < resultadoVectoresSuma.length; i++) {
+            System.out.println(resultadoVectoresSuma[i]);
+        }
+
         System.out.println(sonIguales());
         System.out.println(sumaVectores());
         System.out.println(restaVectores());
-        //System.out.println(invertirArray());
+        System.out.println(invertirArray());
+
+         */
+        //maximoArrayPorParametro(array);
+        invertirArray();//Funcion valor fijo
+        invertirArrayParametro(array);//Procedimiento podemos cambiar los valores
+        invertirArrayParametro(array1);
+        invertirArrayParametro(array2);
 
     }
 
-    public static int maximoArray() {
+    public static void imprimirArray(int[] arrayIntroducido) {
+        //i=0 ; i <= 5
+        for (int i = 0; i <= arrayIntroducido.length - 1; i++) {
+            System.out.println(arrayIntroducido[i]);
+        }
+    }
 
-        Scanner in = new Scanner(System.in);
+    public static int maximoArrayPorParametro(int[] arrayIntroducidoPorParametro) {
+
+        int maximo = arrayIntroducidoPorParametro[0];  // Asigna el primer elemento como máximo inicial
+
+        for (int i = 0; i < arrayIntroducidoPorParametro.length; i++) {
+            if (arrayIntroducidoPorParametro[i] > maximo) {
+                maximo = arrayIntroducidoPorParametro[i];
+            }
+        }
+
+        System.out.println("El máximo entre ellos es :");
+        return maximo;
+    }
+
+    public static int maximoArray() {
         System.out.println("Introduce 5 numeros para saber cual es el maximo entre ellos");
         int[] numIntroducido;
         numIntroducido = new int[5];
@@ -41,8 +92,6 @@ public class MyArray {
     }
 
     public static int minimoArray() {
-
-        Scanner in = new Scanner(System.in);
         System.out.println("Introduce 5 numeros para saber cual es el minimo entre ellos");
         int[] numIntroducido;
         numIntroducido = new int[5];
@@ -63,55 +112,65 @@ public class MyArray {
     }
 
     public static int mediaArray() {
-        Scanner in = new Scanner(System.in);
         System.out.println("Introduce 5 numeros para saber la media entre ellos");
-        int[] numIntroducido;
-        numIntroducido = new int[5];
+        int[] numIntroducido = new int[5];
 
-
+        //Para Añadir valores a mi array
         for (int i = 0; i < numIntroducido.length; i++) {
             numIntroducido[i] = in.nextInt();
         }
+
         int sumaNumeros = numIntroducido[0] + numIntroducido[1] + numIntroducido[2] + numIntroducido[3] + numIntroducido[4];
         int media = sumaNumeros / 5;
-        System.out.println("La media es:");
+
+        int suma = 0;
+        //Acumulando la suma de todos los numeros de mi array
+        for (int i = 0; i < numIntroducido.length; i++) {
+            //
+            suma += numIntroducido[i];
+        }
+        double media1 = suma / (numIntroducido.length - 1);
+
+        System.out.println("La media es:" + media1);
         return media;
     }
 
     public static boolean sonIguales() {
-        Scanner in = new Scanner(System.in);
         System.out.println("Introduce 3 numeros para saber si existen en el programa del 1 al 10");
-        int[] array;
-        array = new int[3];
-        int[] array2;
-        array2 = new int[]{2};
-        boolean coincide = true;
+        int[] array = new int[3];
+        int[] array2 = new int[]{2};
+        boolean coincide = false;
+
+        //Array para que la persona Introduzca 3 numeros
         for (int i = 0; i < array.length; i++) {
             array[i] = in.nextInt();
         }
-        for (int i = 0; i < array2.length; i++) {
-            if (array == array2) {
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i] == array2[0]) {
                 coincide = true;
-                System.out.println("Felicidades el numero coincide");
                 break;
             } else {
                 coincide = false;
-                System.out.println("No coincide");
             }
 
         }
         return coincide;
     }
 
-    public static int sumaVectores() {
-        int[] vector1 = {1, 2, 3, 4};
-        int sumaVector = vector1[0] + vector1[2];
+    public static int[] sumaVectores() {
+        int[] vector1 = {1, 6, 9, 7};
+        int[] vector7 = {6, 5, 4, 3};
+        //for
+        //resultado tiene que tener esto: 7,7,7,7
+        int[] resultado = new int[4];
         System.out.println("esta es la suma " + vector1[0] + " + " + vector1[2]);
-        for (int i = 0; i < vector1.length; i++) {
-            System.out.println(vector1[i] + "");
 
+        for (int i = 0; i < vector1.length; i++) {
+            resultado[i] = vector1[i] + vector7[i];
         }
-        return sumaVector;
+        return resultado;
     }
 
     public static int restaVectores() {
@@ -126,31 +185,48 @@ public class MyArray {
         return restaVector;
     }
 
-   /* public static int invertirArray() {
+    public static void invertirArray() {
 
-        int[] array = {1, 2, 3, 4, 5, 6};
-        int longitud = array.length;
-        for (int i = 0; i < longitud / 2; i++) {
-            int temp = array[i];
-            array[i] = array[longitud - 1 - i];
-            array[longitud - 1 - i] = temp;
+        int[] array = {11, 12, 13, 14, 15, 16};
+        int[] arrayInvertido = new int[6];//Esta lleno de 0
+        int longitud = array.length-1;
+
+        for (int i = longitud, j=0 ; i >= 0  ; i--, j++) {
+            //Sout para ver que tiene el array, lo dejamos comentado porque ya hemos comprobado
+            //que recorremos del ultimo
+            //System.out.println(array[i]);
+            //La variable J del for va servir para recorrer mi  ARRAYAUXILIAR del principio al final
+            arrayInvertido[j] = array[i];
+        }
+
+        //PARA MOSTRAR POR ORDEN MIS DOS ARRAYS
+        for (int i = 0; i < 6 ; i++) {
+            System.out.println("Array Normal "+array[i]);
+            System.out.println("Array Invertido "+arrayInvertido[i]);
 
         }
-        return invertirArray();*/
     }
+    private static int[] invertirArrayParametro(int[] pepe) {
+        //Ponemos el array.lengt porque mi auxiliar va a tener el mismo tamaño que el array
+        //que le pasamos por parametro.
+        int[] arrayInvertido = new int[pepe.length];
+        int longitud = pepe.length-1;
 
+        for (int i = longitud, j=0 ; i >= 0  ; i--, j++) {
+            //Sout para ver que tiene el array, lo dejamos comentado porque ya hemos comprobado
+            //que recorremos del ultimo
+            //System.out.println(array[i]);
+            //La variable J del for va servir para recorrer mi  ARRAYAUXILIAR del principio al final
+            arrayInvertido[j] = pepe[i];
+        }
 
+        //MOSTRAR POR ORDEN MIS DOS ARRAYS
+        for (int i = 0; i < 6 ; i++) {
+            System.out.println("Array Normal "+pepe[i]);
+            System.out.println("Array Invertido "+arrayInvertido[i]);
 
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
+        return arrayInvertido;
+    }
+}
