b1be66fc1813c2bd88e9598bc8f4ebd2239fecec
vinny78 <146063659+vinny78@users.noreply.github.com>
2024-02-06 07:34:05 +0100

    Add files via upload

--- atrapaMoscas.java
+++ atrapaMoscas.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
+
 public class atrapaMoscas {
 
     public static void main(String[] args) {
@@ -9,25 +10,58 @@ public class atrapaMoscas {
 
     public static void atrapamosca() {
         Scanner in = new Scanner(System.in);
+
         boolean posicionMosca = false;
-        int[] eleccionPosicion = new int[1];
-        int[] array = new int[16];
+        //Vas a crear un array de 16 elemntos y tu ejercicio pide 15
+        //int[] array = new int[16]; --> esto esta mal
+        int[] array = new int[15];
         int longitudArray = array.length;
-        int posicionVacia= 0;
+        int posicionVacia = 0;
+
+        //SIRVE PARA SACAR LA POSICION DE LA MOSCA RAMDOM
+        Random random = new Random();
+        //Esto ESTARIA MAL PORQUE LA MOSCA SE VA A POSICIONAR DESDE LA POSCION 0 AL 14 que son
+        //15 elementos contando el 0
+        //int mosca = random.nextInt(1, 15);
+        int mosca = random.nextInt(15);
+        int numeroDedentro = 4;
+
+        array[12]=numeroDedentro;
+
+        //WHILE
+        while (posicionMosca == false) {
         System.out.println("introduce una posicion");
+        int posicionQueNosDicelaPersona = in.nextInt(); //Usuraio mete la posicion del 1 al 15
+        posicionQueNosDicelaPersona = posicionQueNosDicelaPersona - 1; //Aqui restamos 1
+        //Porque el array empieza 0 y termina en 14 no en 15 IMPORTANTE
 
-        for (int i = 0; i < eleccionPosicion.length; i++) {
-            eleccionPosicion[i] = in.nextInt();
-        }
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]=0);
+
+            //Rellenamos el array con ceros.
+            for (int i = 0; i < array.length; i++) {
+                array[i] = 0;
             }
-        Random random=new Random();
-        int movimientoMosca = random.nextInt(1, 15);
-        System.out.println(array[movimientoMosca]=7);
+
+            System.out.println(mosca);
+            array[mosca] = 7;
+            if (posicionQueNosDicelaPersona == mosca) {
+                System.out.println("has ganado has atrapado la mosca");
+                posicionMosca = true;
+            } else if (posicionQueNosDicelaPersona == (mosca - 1) || posicionQueNosDicelaPersona == (mosca + 1)) {
+                System.out.println("La mosca se ha movido");
+                mosca = random.nextInt(15);
+                System.out.println("lA MOSCA SE MUEVE PORQUE ESTAS CERCA, NUEVA POSICION "+mosca);
+            } else if (posicionQueNosDicelaPersona != mosca) {
+                System.out.println("Has fallado prueba otra vez");
+
+            }//Cierre del else if
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("| "+array[i]+" | ");
+            System.out.println("");
+
 
         }
+        }//CIERRE DEL WHILE
+    }//Cierre de la funcion
 
 }
-
-
