85188f20491e8ec3b6194d220933cf8b563a16bd
vinny78 <146063659+vinny78@users.noreply.github.com>
2024-02-07 13:49:52 +0100

    Update wifiHotel.java

--- wifiHotel.java
+++ wifiHotel.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class wifiHotel {
@@ -8,21 +9,25 @@ public class wifiHotel {
     public static void señalHotel(){
         int[] array = new int[20];
         System.out.println("Elige una habitacion para instalar el router");
-        int posicionEligida=4;
+        int  posicionEligida= 4;
         int potenciaRouter=6;
-        int posicionQueNosDicelaPersona = in.nextInt(); //Usuraio mete la posicion del 1 al 20
-        posicionQueNosDicelaPersona = posicionQueNosDicelaPersona - 1;
+
+//        int posicionQueNosDicelaPersona = in.nextInt(); //Usuraio mete la posicion del 1 al 20
+//        posicionQueNosDicelaPersona = posicionQueNosDicelaPersona - 1;
 
         for (int i = 0; i < array.length; i++) {
             array[i] = 0;
         }
-
         array[posicionEligida]=4;
+        System.out.println();
 
-        for (int i = posicionQueNosDicelaPersona; i < array.length; potenciaRouter--) {
-            System.out.println(potenciaRouter);
+
+        for (int i = potenciaRouter; i > 0 ; i--) {
+            array[posicionEligida]=i;
+            System.out.println(i);
+            
         }
-
+        System.out.println(Arrays.toString(array));
     }
 
 }
