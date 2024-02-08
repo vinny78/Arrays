05edd6219558b04383e3938313bb7c32010ae3d3
vinny78 <146063659+vinny78@users.noreply.github.com>
2024-02-08 10:38:21 +0100

    Update wifiHotel.java

--- wifiHotel.java
+++ wifiHotel.java
@@ -5,29 +5,36 @@ public class wifiHotel {
     static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
         señalHotel();
+
     }
     public static void señalHotel(){
         int[] array = new int[20];
-        System.out.println("Elige una habitacion para instalar el router");
-        int  posicionEligida= 4;
-        int potenciaRouter=6;
+        System.out.println("Elige una habitacion para instalar el router,1-20");
+        int  posicionElegida= in.nextInt()-1;
+        int potenciaRouter=3;
+        int posicionInversa=posicionElegida;
 
-//        int posicionQueNosDicelaPersona = in.nextInt(); //Usuraio mete la posicion del 1 al 20
-//        posicionQueNosDicelaPersona = posicionQueNosDicelaPersona - 1;
+        /*int posicionQueNosDicelaPersona = in.nextInt(); //Usuraio mete la posicion del 1 al 20
+       posicionQueNosDicelaPersona = posicionQueNosDicelaPersona - 1;*/
 
         for (int i = 0; i < array.length; i++) {
             array[i] = 0;
         }
-        array[posicionEligida]=4;
-        System.out.println();
+       // array[posicionEligida]=4; fallaba porque me ponia directamente en la posicion 4
+        //System.out.println();
 
 
         for (int i = potenciaRouter; i > 0 ; i--) {
-            array[posicionEligida]=i;
-            System.out.println(i);
-            
+            array[posicionElegida]=i;
+            posicionElegida++;
+
+        }
+        for (int i = potenciaRouter; i >0 ; i--) {
+            array[posicionInversa]=i;
+            posicionInversa--;
         }
         System.out.println(Arrays.toString(array));
     }
 
-}
+    }
+
