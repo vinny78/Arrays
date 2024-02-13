27356ac71a6c6808de0fce041e8598bec618c588
vinny78 <146063659+vinny78@users.noreply.github.com>
2024-02-13 10:42:46 +0100

    Update buscaPareja.java

--- buscaPareja.java
+++ buscaPareja.java
@@ -5,24 +5,35 @@ import java.util.Random;
 public class buscaPareja {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-parejas();
+        parejas();
     }
 
 
-   public static void  parejas(){
-       String []array=new String[20];
-       String[] animales = {"León", "halcón", "cocodrilo", "tiburón", "elefante",
-               "tigre", "perro", "gato", "serpiente", "paloma"};
-       Random posicionAnimales= new Random();
+    public static void parejas() {
+        Scanner in = new Scanner(System.in);
+        //array parejas
+        String[] arrayParejas = new String[20];
+        //arra posiciones
+        int[] PosicionesAnimales = new int[20];
 
-       for (int i = 0; i < 20; i++) {
-           array[i] = animales[i % animales.length];
-       }
+        String[] animales = {"León", "halcón", "cocodrilo", "tiburón", "elefante",
+                "tigre", "perro", "gato", "serpiente", "paloma"};
+        Random random = new Random();
+        int posicionElegidaJugador = in.nextInt();
+        int posicionElegidaJugador2 = in.nextInt();
+        
+        boolean posicionAnimales = false;
+        //bucle de las parejas
+        for (int i = 0; i < 20; i++) {
+            arrayParejas[i] = animales[i % animales.length];
+          
+        }
+       
 
+        //esto imprime el array del String
+       /* for (String animal : arrayParejas) {
+            System.out.println(animal);*/
+        }
+    }
 
-       /*for (String animal:array) {
-           System.out.println(animal);*/
-       }
-   }
-
-}
+//}
